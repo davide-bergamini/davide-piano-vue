03fbc8c5ea2a1dcea5c66e88f4bc7d937e985c01
Davide Bergamini <davide.bergamini@ymail.com>
2026-07-01 16:54:49 +0200

    Tolto il link visivo della sezione I MIEI CD

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,7 @@
     <nav class="top-nav">
       <RouterLink to="/mozart"> Musica classica </RouterLink>
 
-      <RouterLink to="/timeline"> Timeline </RouterLink>
-
-      <RouterLink class="nav-pill" to="/discoteca"> I miei CD </RouterLink>
+      <RouterLink to="/timeline"> Il mio percorso </RouterLink>
 
       <!-- <a href="#"> Musica jazz </a> -->
     </nav>
